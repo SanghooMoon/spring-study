e2af81c06884cc008d83bc79b164247ad619be00
sanghoomoon <sanghoo.moon@gmail.com>
2023-11-21 21:09:02 +0900

    23-11-21 실습 코드

---
+++ src/springbook/user/dao/DUserDao.java
@@ -0,0 +1,12 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DUserDao extends UserDao{
+
+    @Override
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        return null;
+    }
+}
